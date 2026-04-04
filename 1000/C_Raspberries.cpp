b8e0d78315a3a50ca8870ea5d918b41c7c4aa964
Ryukon07 <b25410@students.iitmandi.ac.in>
2026-04-04 22:59:19 +0530

    Did 1000 rating one question for checking

--- 1000/C_Raspberries.cpp
+++ 1000/C_Raspberries.cpp
@@ -4,18 +4,39 @@
 
 void solve(){
     int n, k; std::cin >> n >> k;
-    std::vector<int> a(n), forMin(n);
+    std::vector<int> a(n), minNeeded(n);
+
+    int evenCount = 0;
+
     for(int i = 0; i < n; i++){
         std::cin >> a[i];
+        
         if(a[i] % k == 0){
-            forMin[i] = 0;
+            minNeeded[i] = 0;
         }else{
-            forMin[i] = k - (a[i] % k);
+            minNeeded[i] = k - (a[i] % k);
+        }
+
+        if(a[i] % 2 == 0){
+            evenCount++;
+        }
+
+    }
+
+    int ans = *std::min_element(minNeeded.begin(), minNeeded.end());
+
+    if(k == 4){
+        if(evenCount >= 2){
+            ans = std::min(ans, 0);
+        }else if (evenCount == 1){
+            ans = std::min(ans, 1);
+        }else if (evenCount == 0){
+            ans = std::min(ans, 2);
         }
     }
 
-    int ans = *std::min_element(forMin.begin(), forMin.end());
     std::cout << ans << std::endl;
+
 }
 
 int main(){
